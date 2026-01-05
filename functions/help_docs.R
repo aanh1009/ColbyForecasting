85133ea6100b32bcaba8dae625139e009c5fb942
Ben Tupper <btupper@bigelow.org>
2026-01-05 08:50:19 -0500

    fix help doc path

--- functions/help_docs.R
+++ functions/help_docs.R
@@ -2,7 +2,7 @@
 # which includes pushing onto github at https://github.com/BigelowLab/ColbyForecastingDocs
 #
 # It only makes sense to use if you have push rights to that repos
-update_help_docs = function(path = "~/Library/CloudStorage/Dropbox/code/projects/ColbyForecastingDocs"){
+update_help_docs = function(path = "/mnt/ecocast/projects/ColbyForecasting/ColbyForecastingDocs"){
   source(file.path(path, "inst/update_script.R"))
 }
 
